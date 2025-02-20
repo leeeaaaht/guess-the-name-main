8f135c9b18106d204f8947b0291a1b7713db3e94
leah_t <ltowery17@yahoo.com>
2025-02-20 12:30:07 -0600

    added guessed letters

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const hiddenButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const symbol = function(word){
 const letterRepresent = [];
@@ -21,7 +22,36 @@ symbol(word);
 
 button.addEventListener("click", function (e){
     e.preventDefault();
+    message.innerText = "";
     const guessLetter = letterInput.value;
     console.log(guessLetter);
+    const goodGuess = playerInput(guess);
+
+    if(goodGuess) {
+        makeGuess(guess);
+    }
     letterInput.value = "";
 });
+
+const playerInput = function(input){
+    const acceptedLetter = /[a-zA-Z]/;
+    if(input.length === 0){
+        message.innerText = 'Enter a letter.';
+    } else if(input.length > 1){
+        message.innerText = 'You need at least one letter.';
+    } else if (!input.match(acceptedLetter)){
+     message.innerText = 'Enter a letter from A to Z.';
+    } else{
+        return input;
+    }
+};
+
+const makeGuess = function(guess){
+    guess = guess.toUpperCase();
+    if(guessedLetters.includes(guess)){
+        message.innerText = 'You chose that letter already,try again!'
+    } else{
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
